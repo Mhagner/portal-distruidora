fa914e06783d6c98f2d57c0333358fe2c866f288
portaldistribuidora <88737083+portaldistribuidora@users.noreply.github.com>
2021-11-03 11:50:24 -0300

    Update FooterSocial.js

--- src/components/Footer/FooterSocial.js
+++ src/components/Footer/FooterSocial.js
@@ -1,9 +1,9 @@
 export function FooterSocial({ urlInstagram, urlFacebook, urlWhatsapp }) {
     return (
         <div className="footer-social">
-            <a href={urlFacebook || '#'}><i className="fa fa-facebook" /></a>
-            <a href={urlInstagram || '#'}><i className="fa fa-instagram" /></a>
-            <a href={urlWhatsapp || '#'}><i className="fa fa-whatsapp" /></a>
+            <a href={urlFacebook || 'https://pt-br.facebook.com/portaldistribuidora/'}><i className="fa fa-facebook" /></a>
+            <a href={urlInstagram || 'https://www.instagram.com/distribuidoraportal/'}><i className="fa fa-instagram" /></a>
+            <a href={urlWhatsapp || 'https://api.whatsapp.com/message/IH3AWX7MICS5I1'}><i className="fa fa-whatsapp" /></a>
         </div>
     )
 }
